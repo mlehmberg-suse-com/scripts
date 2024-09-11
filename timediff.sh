986cf428868d5adae35b08ded5a1c2de2e27164e
Marc Lehmberg <marc.lehmberg@suse.com>
2024-09-11 09:08:43 +0800

    Update timediff.sh

--- timediff.sh
+++ timediff.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #ARG=${1? Error. Please, provide the input file. Example: timediff.sh  filename}
-echo " THIS SCRIPT CAN TAKE SOMETIME This script will process all sessions created on the server and output a csv file for  analysis of any delays in login times, etc, a session can of course, take a long but automated session on HANA should complete quickly"
+echo " THIS SCRIPT CAN TAKE SOMETIME This script will process all sessions created on the server and ouput a csv fiel analysis of any delaysin login times etc, session can of course take a long but automated sesison on HANA shpould complete quickly"
 inputfile=messages-20240819.txt
 session_file=session.txt
 outputfile=$inputfile-timedelays.csv
 tempfile=temp.txt
 function create_input () {
 # echo "Processing $inputfile to temp file"
-grep '.scope: Deactivated successfully.\|Started Session' $inputfile |sed 's/session-/End session-/g'  | sed 's/Started Session /Start session-/g' | sed -e 's/azlsapd9pdb01 systemd\[1\]://g'| sed 's/.scope:/ scope:/g'  > $tempfile
+grep '.scope: Deactivated successfully.\|Started Session' $inputfile |sed 's/session-/End session-/g'  | sed 's/Started Session /Start session-/g' | sed -e 's/azlsapd9pdb02 systemd\[1\]://g'| sed 's/.scope:/ scope:/g'  > $tempfile
 
 #echo " Sorting file"
-# sort -o -b -k3 $tempfile
+# sort -o -b -k3 $tempfile 
 }
 
 function session_file (){
@@ -19,9 +19,9 @@ rm $session_file
 grep session- $tempfile | cut -d " " -f4 | cut -d "-" -f 2| uniq > $session_file
 }
 function session_array () {
-# get all sessions not used
+# get all sessions not used 
 readarray -t my_array  < <( grep session- $tempfile | cut -d " " -f4 | cut -d "-" -f 2| uniq)
-#debug check array values
+#debug check array values 
 #printf '%s\n' "${my_array[@]}"
 }
 
@@ -34,27 +34,27 @@ echo login session,start time, end time , elapsed time in sec >> $outputfile
 }
 
 function process_session () {
-cat $session_file | while read line
+cat $session_file | while read line   
 #for value in "${my_array[@]}"
-        do
-        echo "Processing session $line"
-        #get start end and end time
-        start=$(grep $line $tempfile | grep Start  | cut -d " " -f 1 |  sed 's/T/ /g' | cut -d . -f 1)
-        start_epoch=$(date -d "${start}" +"%s")
-        end=$(grep $line $tempfile | grep End  | cut -d " " -f 1 | sed 's/T/ /g' |  cut -d . -f 1)
-        end_epoch=$(date -d "${end}" +"%s")
-        user_name=$(grep $line $tempfile | grep Start  | cut -d " " -f 7  )
-        #debug
-        #echo $start
-        #echo $start_epoch
-        #echo $end
-        #echo $end_epoch
-        #echo $session
-        time_elapsed=$(($end_epoch-$start_epoch))
-        #f [ $time_elapsed -gt 0 ]; then
-        #fi
-        echo "$line,$user_name,$start,$end,$time_elapsed" >> $outputfile
-        done
+	do
+	echo "Processing session $line"
+	#get start end and end time
+	start=$(grep $line $tempfile | grep Start  | cut -d " " -f 1 |  sed 's/T/ /g' | cut -d . -f 1)
+	start_epoch=$(date -d "${start}" +"%s") 
+	end=$(grep $line $tempfile | grep End  | cut -d " " -f 1 | sed 's/T/ /g' |  cut -d . -f 1)
+	end_epoch=$(date -d "${end}" +"%s")
+	user_name=$(grep $line $tempfile | grep Start  | cut -d " " -f 7  )
+	#debug
+	#echo $start
+	#echo $start_epoch
+	#echo $end 
+	#echo $end_epoch
+	#echo $session
+	time_elapsed=$(($end_epoch-$start_epoch)) 
+	#f [ $time_elapsed -gt 0 ]; then
+	#fi
+	echo "$line,$user_name,$start,$end,$time_elapsed" >> $outputfile
+	done 
 }
 
 #main
